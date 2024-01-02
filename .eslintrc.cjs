ac8cdf6566fe5ff17ebc740c00e446e140e72bb0
David Johnston <davidajohnston2000@gmail.com>
2024-01-02 23:14:39 +0000

    AirBnB

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,31 @@
 module.exports = {
   root: true,
   env: { browser: true, es2022: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended', 'prettier'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vitest.config.ts', 'vite.config.ts', 'vitest-setup.ts'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   plugins: ['react-refresh'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.test.{ts,tsx}', '**/*.test.helpers.{ts,tsx}'],
+        optionalDependencies: false,
+      },
+    ],
   },
 };
