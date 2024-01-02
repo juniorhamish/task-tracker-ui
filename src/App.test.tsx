f5d70e53eabd58b0947d6b44d820217fa2b76f39
David Johnston <david@dajohnston.co.uk>
2024-01-02 21:19:19 +0000

    Update App.test.tsx (#3)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,13 @@
 import App from "./App.tsx";
 import {render, screen} from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
 
 describe('App', () => {
-    it('Render', () => {
+    it('Render', async () => {
         render(<App/>);
 
-        expect(screen.getByRole('button')).toBeInTheDocument();
+        await userEvent.click(screen.getByRole('button'));
+
+        expect(screen.getByRole('button', {name: 'count is 1'})).toBeInTheDocument();
     });
 });
