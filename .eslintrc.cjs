33dc1f7668d5d7900541d3780c58a161281bcf6b
David Johnston <david@dajohnston.co.uk>
2024-01-02 23:26:26 +0000

    Es lint prettier (#10)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,31 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  env: { browser: true, es2022: true },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vitest.config.ts', 'vite.config.ts', 'vitest-setup.ts'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   plugins: ['react-refresh'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.test.{ts,tsx}', '**/*.test.helpers.{ts,tsx}'],
+        optionalDependencies: false,
+      },
+    ],
   },
 };
