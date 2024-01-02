8ae85f7e8f6fc0636d5d4f01a7caf18a16f3e2a0
David Johnston <davidajohnston2000@gmail.com>
2024-01-02 22:25:40 +0000

    Use prettier

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,13 @@
-import App from "./App.tsx";
-import {render, screen} from "@testing-library/react";
-import userEvent from '@testing-library/user-event'
+import App from './App.tsx';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('App', () => {
-    it('Render', async () => {
-        render(<App/>);
+  it('Render', async () => {
+    render(<App />);
 
-        await userEvent.click(screen.getByRole('button'));
+    await userEvent.click(screen.getByRole('button'));
 
-        expect(screen.getByRole('button', {name: 'count is 1'})).toBeInTheDocument();
-    });
+    expect(screen.getByRole('button', { name: 'count is 1' })).toBeInTheDocument();
+  });
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,15 @@
 // vitest.config.ts
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: ['./vitest-setup.ts'],
-        coverage: {
-            provider: 'istanbul',
-            reporter: ['json', 'html', 'lcov'],
-            exclude: ["src/main.tsx"],
-        },
-    }
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest-setup.ts'],
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['json', 'html', 'lcov'],
+      exclude: ['src/main.tsx'],
+    },
+  },
+});
