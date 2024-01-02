f968f954a5ecd0e4a489ffd7fb73ac57a6ad4521
David Johnston <davidajohnston2000@gmail.com>
2024-01-02 14:25:00 +0000

    Include coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,8 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom',
         setupFiles: ['./vitest-setup.ts'],
+        coverage: {
+            provider: 'istanbul'
+        },
     }
 })
