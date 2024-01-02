ecfeac830736a36878db9dbc8f53ee80ce2e5e81
David Johnston <david@dajohnston.co.uk>
2024-01-02 14:33:19 +0000

    Add github action to build and test (#1)

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+// vitest.config.ts
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: ['./vitest-setup.ts'],
+        coverage: {
+            provider: 'istanbul',
+            reporter: ['json', 'html'],
+        },
+    }
+})
