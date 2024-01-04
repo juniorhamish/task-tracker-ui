c377aa0aacbedd63857f5ba0019a4b45d9bc90b0
David Johnston <david@dajohnston.co.uk>
2024-01-04 16:21:26 +0000

    More strict linting (#12)

--- src/app.tsx
+++ src/app.tsx
@@ -18,7 +18,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button type="button" onClick={() => setCount((x) => x + 1)}>
+        <button
+          type="button"
+          onClick={() => {
+            setCount((x) => x + 1);
+          }}
+        >
           count is {count}
         </button>
         <p>
