7bebf200d639d1f8ab7beaa9fbd535ce8fb07c17
David Johnston <davidajohnston2000@gmail.com>
2024-08-17 15:25:42 +0100

    Upgrade typescript

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
 // vitest.config.ts
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
