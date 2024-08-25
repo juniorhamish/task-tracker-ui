f4341c4de4859d3698e12b3c324ad625f8c6eeae
David Johnston <davidajohnston2000@gmail.com>
2024-08-25 19:44:09 +0100

    Change the logo

--- src/components/appbar/AppBar.tsx
+++ src/components/appbar/AppBar.tsx
@@ -1,4 +1,5 @@
 import { AppBar as MuiAppBar, Box, Button, Toolbar } from '@mui/material';
+import logo from '../../assets/logo.png';
 
 interface Props {
   onLogin: () => void;
@@ -10,7 +11,7 @@ export default function AppBar({ onLogin, onLogout, isAuthenticated }: Props) {
   return (
     <MuiAppBar position="static">
       <Toolbar>
-        <Box sx={{ ml: 'auto' }} />
+        <Box sx={{ mr: 'auto', maxWidth: 50 }} component="img" src={logo} alt="Task Tracker logo" />
         {!isAuthenticated ? (
           <Button color="inherit" onClick={onLogin}>
             Login
