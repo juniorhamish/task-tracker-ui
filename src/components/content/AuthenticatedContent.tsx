982d4e320240d430170bcc8421620028130b90aa
David Johnston <davidajohnston2000@gmail.com>
2024-08-26 22:22:56 +0100

    Extract interface to types

--- src/components/content/AuthenticatedContent.tsx
+++ src/components/content/AuthenticatedContent.tsx
@@ -1,9 +1,4 @@
-export interface User {
-  givenName?: string;
-  familyName?: string;
-  name?: string;
-  picture?: string;
-}
+import { User } from '../../common/types';
 
 export default function AuthenticatedContent({ user }: { user: User }) {
   return <h2>{`${user.givenName ?? ''} ${user.familyName ?? ''}`}</h2>;
