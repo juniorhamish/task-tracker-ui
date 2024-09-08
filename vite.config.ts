0ae734141f295a3ab759e4963e40608dbb0c86aa
David Johnston <davidajohnston2000@gmail.com>
2024-09-09 00:48:02 +0100

    Log proxy errors

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-  console.log(process.env);
   return defineConfig({
     plugins: [react()],
     server: {
@@ -14,6 +13,32 @@ export default ({ mode }: { mode: string }) => {
           target: process.env.VITE_API_BASE_URL ?? 'http://localhost:8080',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
+          configure: (proxy, _options) => {
+            proxy.on('error', (err, _req, _res) => {
+              console.log('proxy error', err);
+            });
+            proxy.on('proxyReq', (proxyReq, req, _res) => {
+              console.log(
+                'Sending Request:',
+                req.method,
+                req.url,
+                ' => TO THE TARGET =>  ',
+                proxyReq.method,
+                proxyReq.protocol,
+                proxyReq.host,
+                proxyReq.path,
+                JSON.stringify(proxyReq.getHeaders()),
+              );
+            });
+            proxy.on('proxyRes', (proxyRes, req, _res) => {
+              console.log(
+                'Received Response from the Target:',
+                proxyRes.statusCode,
+                req.url,
+                JSON.stringify(proxyRes.headers),
+              );
+            });
+          },
         },
       },
     },
