d605a6943f7379e104a45a9d8f5b33133d8fdbf4
David Johnston <davidajohnston2000@gmail.com>
2024-09-09 21:49:42 +0100

    Remove proxy logging

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,16 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default ({ mode }: { mode: string }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  console.log(process.env.VITE_API_BASE_URL);
-  return defineConfig({
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: process.env.VITE_API_BASE_URL ?? 'http://localhost:8080',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-          configure: (proxy, _options) => {
-            proxy.on('error', (err, _req, _res) => {
-              console.log('proxy error', err);
-            });
-            proxy.on('proxyReq', (proxyReq, req, _res) => {
-              console.log(
-                'Sending Request:',
-                req.method,
-                req.url,
-                ' => TO THE TARGET =>  ',
-                proxyReq.method,
-                proxyReq.protocol,
-                proxyReq.host,
-                proxyReq.path,
-                JSON.stringify(proxyReq.getHeaders()),
-              );
-            });
-            proxy.on('proxyRes', (proxyRes, req, _res) => {
-              console.log(
-                'Received Response from the Target:',
-                proxyRes.statusCode,
-                req.url,
-                JSON.stringify(proxyRes.headers),
-              );
-            });
-          },
-        },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-  });
-};
+  },
+});
