7288256c91fd98493cc42473b66ebc44d8e8ec82
David Johnston <david@dajohnston.co.uk>
2024-09-27 23:14:14 +0100

    Log error then use popup to get token (#123)

--- src/components/Auth0Initializer.test.tsx
+++ src/components/Auth0Initializer.test.tsx
@@ -2,23 +2,31 @@ import { client, UserInfoService } from '../gen/client';
 import { Auth0ContextInterface, useAuth0 } from '@auth0/auth0-react';
 import Auth0Initializer from './Auth0Initializer.tsx';
 import { render } from '@testing-library/react';
+import { log } from '../logging/Log.ts';
 
 vi.mock('@auth0/auth0-react');
 
 describe('request headers', () => {
   let getAccessTokenSilently: () => Promise<string>;
+  let getAccessTokenWithPopup: () => Promise<string>;
   let setRequestHeader: () => void;
   beforeEach(() => {
     getAccessTokenSilently = vi.fn();
-    vi.mocked(useAuth0).mockReturnValue({ getAccessTokenSilently } as unknown as Auth0ContextInterface);
+    getAccessTokenWithPopup = vi.fn();
+    vi.mocked(useAuth0).mockReturnValue({
+      getAccessTokenSilently,
+      getAccessTokenWithPopup,
+    } as unknown as Auth0ContextInterface);
     setRequestHeader = vi.fn();
+    const doSend = () => Promise.resolve();
     const xhrMock: Partial<XMLHttpRequest> = {
       open: vi.fn(),
-      send: Promise.resolve,
+      send: void doSend(),
       setRequestHeader,
     };
     vi.spyOn(window, 'XMLHttpRequest').mockImplementation(() => xhrMock as XMLHttpRequest);
     vi.spyOn(client, 'setConfig');
+    vi.spyOn(log, 'error');
   });
   afterEach(() => {
     vi.resetAllMocks();
@@ -30,6 +38,17 @@ describe('request headers', () => {
     await UserInfoService.get();
 
     expect(setRequestHeader).toHaveBeenCalledWith('Authorization', 'Bearer Token');
+  });
+  it('should use the popup if it fails to get the token silently', async () => {
+    render(<Auth0Initializer />);
+    const error = new Error('Failed to get token silently');
+    vi.mocked(getAccessTokenSilently).mockRejectedValueOnce(error);
+    vi.mocked(getAccessTokenWithPopup).mockResolvedValueOnce('Token From Popup');
+
+    await UserInfoService.get();
+
+    expect(setRequestHeader).toHaveBeenCalledWith('Authorization', 'Bearer Token From Popup');
+    expect(log.error).toHaveBeenCalledWith('Failed to get token silently.', error);
   });
   it('should set the base URL', async () => {
     render(<Auth0Initializer />);
--- src/components/Auth0Initializer.tsx
+++ src/components/Auth0Initializer.tsx
@@ -1,16 +1,25 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { useEffect } from 'react';
 import { client } from '../gen/client';
+import { log } from '../logging/Log.ts';
+import { Context } from '@logtail/types';
 
 export default function Auth0Initializer() {
-  const { getAccessTokenSilently } = useAuth0();
+  const { getAccessTokenSilently, getAccessTokenWithPopup } = useAuth0();
   useEffect(() => {
     client.setConfig({ baseURL: '/api' });
-    client.instance.interceptors.request.use(async (config) => {
-      const token = await getAccessTokenSilently();
+    const addAuthorizationHeader = client.instance.interceptors.request.use(async (config) => {
+      let token: string | undefined;
+      try {
+        token = await getAccessTokenSilently();
+      } catch (e) {
+        await log.error('Failed to get token silently.', e as Context);
+        token = await getAccessTokenWithPopup();
+      }
       config.headers.Authorization = `Bearer ${token}`;
       return config;
     });
-  }, [getAccessTokenSilently, client]);
+    return () => client.instance.interceptors.request.eject(addAuthorizationHeader);
+  }, [getAccessTokenSilently, getAccessTokenWithPopup, client]);
   return <></>;
 }
