c98913cece4f771fe58c10f1420a045aa482b66a
David Johnston <david@dajohnston.co.uk>
2024-10-01 23:15:29 +0100

    Remove prettier in favour of Stylistic (#135)

--- eslint.config.js
+++ eslint.config.js
@@ -4,10 +4,10 @@ import pluginReact from 'eslint-plugin-react';
 import pluginReactRefresh from 'eslint-plugin-react-refresh';
 import pluginEslintComments from 'eslint-plugin-eslint-comments';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import pluginPromise from 'eslint-plugin-promise';
 import stylistic from '@stylistic/eslint-plugin';
 import reactHooks from 'eslint-plugin-react-hooks';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
@@ -15,32 +15,28 @@ export default [
   { ignores: ['src/vite-env.d.ts', 'dist', 'vitest.config.ts', 'vite.config.ts', 'coverage', '!.storybook'] },
   pluginJs.configs.recommended,
   ...pluginTSESLint.configs.strict,
-  ...pluginTSESLint.configs.stylistic,
   pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
   jsxA11y.flatConfigs.recommended,
   pluginPromise.configs['flat/recommended'],
   stylistic.configs.customize({
     semi: true,
     braceStyle: '1tbs',
-    arrowParens: true,
   }),
   {
     plugins: {
       'react-refresh': pluginReactRefresh,
       'eslint-comments': pluginEslintComments,
       'react-hooks': reactHooks,
+      typescriptEslint,
     },
     rules: {
       'no-console': 'warn',
-      'react/react-in-jsx-scope': 'off',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'react/require-default-props': ['off'],
-      '@stylistic/multiline-ternary': ['off'],
-      '@stylistic/jsx-wrap-multilines': ['off'],
+      '@stylistic/operator-linebreak': ['error', 'after', { overrides: { '?': 'before', ':': 'before' } }],
       ...reactHooks.configs.recommended.rules,
     },
   },
-  eslintPluginPrettierRecommended,
   { files: ['**/gen'] },
   { rules: { '@typescript-eslint/no-extraneous-class': 'off' } },
 ];
